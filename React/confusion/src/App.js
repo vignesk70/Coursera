48ad1de344ba5dc55e61ce02975e53f2750fd665
Vigneswaren Krishnamoorthy <vignes_k@yahoo.com>
2021-06-05 15:44:14 +0800

    confusion starter

---
+++ React/confusion/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Navbar, NavbarBrand} from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="secondary">
+        <div className='container'>
+          <NavbarBrand href="/">My Ristorante</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
