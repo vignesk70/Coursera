d880fba4387e369e999e054e9e6498bdf0b703cf
Vigneswaren Krishnamoorthy <vignes_k@yahoo.com>
2021-06-06 11:07:30 +0800

    Components Part 2

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,17 +1,30 @@
+import {Component} from 'react'
 import Menu from './components/MenuComponent'
 import {Navbar, NavbarBrand} from 'reactstrap'
+import {DISHES} from './shared/dishes'
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+  render(){
   return (
     <div className="App">
-      <Navbar dark color="secondary">
+      <Navbar dark color="primary">
         <div className='container'>
           <NavbarBrand href="/">My Ristorante</NavbarBrand>
         </div>
       </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
