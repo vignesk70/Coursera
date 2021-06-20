5f9f9a1583803fce3e5247d9f535101eab4d0974
Vigneswaren Krishnamoorthy <vignes_k@yahoo.com>
2021-06-20 11:14:43 +0800

    Graded assignment 3

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -3,12 +3,9 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   Button,
-  FormGroup,
   Label,
   Row,
-  Input,
   Col,
-  FormFeedback,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -33,81 +30,129 @@ function FormSubmit() {
         if (!values.firstname) {
           errors.firstname = "First Name is Required";
         } else if (values.firstname.length < minlength) {
-            errors.firstname = "Must be greater than " + (minlength-1)+ " characters";
+          errors.firstname =
+            "Must be greater than " + (minlength - 1) + " characters";
         } else if (values.firstname.length > maxlength) {
-            errors.firstname = "Must be less than " + (maxlength)+ " characters";
+          errors.firstname = "Must be less than " + maxlength + " characters";
         }
         if (!values.lastname) {
           errors.lastname = " Last name is required";
-        }else if (values.lastname.length < minlength) {
-            errors.lastname = "Too Short"
+        } else if (values.lastname.length < minlength) {
+          errors.lastname = "Too Short";
         } else if (values.lastname.length > maxlength) {
-            errors.lastname = "Too long"
+          errors.lastname = "Too long";
         }
         if (!values.telnum) {
           errors.telnum = " Telephone number is required";
-        } else if (isNaN(values.telnum)) { errors.telnum = " Please put numbers only"}
+        } else if (isNaN(values.telnum)) {
+          errors.telnum = " Please put numbers only";
+        }
         if (!values.email) {
           errors.email = " Email is required";
-        }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-          errors.email = 'Invalid email address';}
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
 
         return errors;
       }}
       onSubmit={async (values) => {
         await new Promise((r) => setTimeout(r, 500));
-        alert(JSON.stringify(values, null, 2));}}
+        alert(JSON.stringify(values, null, 2));
+      }}
     >
       {({ isSubmitting }) => (
         <Form>
-          <Row> <Label htmlFor="firstname" md={2}>
-                First Name
-              </Label>
+          <Row>
+            {" "}
+            <Label htmlFor="firstname" md={2}>
+              First Name
+            </Label>
             <Col sm={10}>
               <Field type="text" name="firstname" />
-              <ErrorMessage className="text-danger" name="firstname" component="div" />
+              <ErrorMessage
+                className="text-danger"
+                name="firstname"
+                component="div"
+              />
             </Col>
           </Row>
-          <Row> <Label htmlFor="lastname" md={2}>
-                Last Name
-              </Label>
-              <Col sm={10}>
-          <Field type="text" name="lastname" />
-          <ErrorMessage className="text-danger" name="lastname" component="div" /> </Col>
+          <Row>
+            {" "}
+            <Label htmlFor="lastname" md={2}>
+              Last Name
+            </Label>
+            <Col sm={10}>
+              <Field type="text" name="lastname" />
+              <ErrorMessage
+                className="text-danger"
+                name="lastname"
+                component="div"
+              />{" "}
+            </Col>
           </Row>
-          <Row> <Label htmlFor="telnum" md={2}>
-                Tel. Number
-              </Label>
-              <Col sm={10}>
-          <Field type="text" name="telnum" />
-          <ErrorMessage className="text-danger" name="telnum" component="div" /> </Col>
+          <Row>
+            {" "}
+            <Label htmlFor="telnum" md={2}>
+              Tel. Number
+            </Label>
+            <Col sm={10}>
+              <Field type="text" name="telnum" />
+              <ErrorMessage
+                className="text-danger"
+                name="telnum"
+                component="div"
+              />{" "}
+            </Col>
           </Row>
-          <Row> <Label htmlFor="telnum" md={2}>
-                Email
-              </Label>
-              <Col sm={10}>
-          <Field type="email" name="email" />
-          <ErrorMessage className="text-danger" name="email" component="div" /> </Col>
+          <Row>
+            {" "}
+            <Label htmlFor="telnum" md={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Field type="email" name="email" />
+              <ErrorMessage
+                className="text-danger"
+                name="email"
+                component="div"
+              />{" "}
+            </Col>
           </Row>
-          <Row> <Label htmlFor="telnum" md={2}>
-                May we contact you?
-              </Label>
-              <Col sm={2}>
-          <Field type="checkbox" name="agree" />
-          <ErrorMessage className="text-danger" name="agree" component="div" /> </Col>
-
-              <Col sm={10}>
-          <Field as="select" name="contactType">
-            <option value="Tel.">Tel</option>
-            <option value="Email">Email</option>
-          </Field></Col>
+          <Row>
+            {" "}
+            <Label htmlFor="telnum" md={2}>
+              May we contact you?
+            </Label>
+            <Col sm={2}>
+              <Field type="checkbox" name="agree" />
+              <ErrorMessage
+                className="text-danger"
+                name="agree"
+                component="div"
+              />{" "}
+            </Col>
+            <Col sm={10}>
+              <Field as="select" name="contactType">
+                <option value="Tel.">Tel</option>
+                <option value="Email">Email</option>
+              </Field>
+            </Col>
           </Row>
-          <Row> <Label htmlFor="message" md={2}>
-                Feedback
-              </Label>
-              <Col md={{ size: 10, offset: 2 }}>
-          <Field type="textarea" rows="12" name="message" />
-          <ErrorMessage className="text-danger" name="message" component="div" /> </Col>
+          <Row>
+            {" "}
+            <Label htmlFor="message" md={2}>
+              Feedback
+            </Label>
+            <Col md={{ size: 10, offset: 2 }}>
+              <Field type="textarea" rows="12" name="message" />
+              <ErrorMessage
+                className="text-danger"
+                name="message"
+                component="div"
+              />{" "}
+            </Col>
           </Row>
           <Button color="primary" type="submit" disabled={isSubmitting}>
             Submit
@@ -247,7 +292,11 @@ class Contact extends Component {
               >
                 <i className="fa fa-phone"></i> Call
               </a>
-              <a role="button" className="btn btn-info" href="skype:+85212345678">
+              <a
+                role="button"
+                className="btn btn-info"
+                href="skype:+85212345678"
+              >
                 <i className="fa fa-skype"></i> Skype
               </a>
               <a
@@ -265,9 +314,7 @@ class Contact extends Component {
             <h3>Send us your feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-
             <FormSubmit />
-
           </div>
         </div>
       </div>
--- React/confusion/src/components/DishDetailComponent.js
+++ React/confusion/src/components/DishDetailComponent.js
@@ -1,125 +1,273 @@
-import React from 'react'
+import React from "react";
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem
-} from 'reactstrap';
-import {Link} from 'react-router-dom';
+  Button,
+  Label,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+  Col,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-edit" /> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            {/* <Formik
+              initialValues={{
+                rating: "1",
+                username: "",
+                comment: "",
+              }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.username) {
+                  errors.firstname = "First Name is Required";
+                } else if (values.username.length < this.minlength) {
+                  errors.username =
+                    "Must be greater than " +
+                    (this.minlength - 1) +
+                    " characters";
+                } else if (values.username.length > this.maxlength) {
+                  errors.username =
+                    "Must be less than " + this.maxlength + " characters";
+                }
+
+                return errors;
+              }}
+              onSubmit={async (values) => {
+                await new Promise((r) => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <Form>
+                    <Row>
+                      <Col>
+                        <Label htmlFor="rating">Your Name</Label>
+                        <Field as="select" id="rating" name="rating">
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                        </Field>
+                      </Col>
+                    </Row>
+                    <Label htmlFor="username">Username</Label>
+                    <Field type="text" id="username" name="username"></Field>
+                    <Label htmlFor="comment">Comment</Label>
+                    <Field type="text" id="comment" name="comment"></Field>
+                  </Form>
+                  <Button color="primary" type="submit" disabled={isSubmitting}>
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik> */}
+            <Formik
+              initialValues={{
+                username: "",
+                rating: "1",
+                comment: "",
+              }}
+              validate={(values) => {
+                const errors = {};
+                const maxlength=15;
+                const minlength= 3;
+                console.log(minlength,maxlength)
+                if (!values.username) {
+                  errors.username = "User Name is Required";
+                } else if (values.username.length < minlength) {
+                  errors.username =
+                    "Must be greater than " +
+                    (minlength - 1) +
+                    " characters";
+                } else if (values.username.length > maxlength) {
+                  errors.username =
+                    "Must be less than " + maxlength + " characters";
+                }
+
+                return errors;
+              }}
+              onSubmit={async (values) => {
+                await new Promise((r) => setTimeout(r, 500));
+                alert(JSON.stringify(values, null, 2));
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <Row>
+                    <Label htmlFor="rating" md={2}>
+                      Rating
+                    </Label>
+                      <Field as="select" name="rating">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </Field>
+                  </Row>
+                  <Row>
+
+                    <Label htmlFor="username" md={2}>
+                      Username
+                    </Label>
+                    {/* <Col sm={10}> */}
+                      <Field type="text" name="username" />
+                      <ErrorMessage
+                        className="text-danger"
+                        name="username"
+                        component="div"
+                      />
+                    {/* </Col> */}
+                  </Row>
+
+                  <Row>
+                    <Label htmlFor="message" md={2}>
+                      Comment
+                    </Label>
+                    {/* <Col md={{ size: 10, offset: 2 }}> */}
+                      <Field className="text-area" as="textarea" rows="12" name="comment" />
+
+                    {/* </Col> */}
+                  </Row>
+                  <br/>
+                  <Button color="primary" type="submit" disabled={isSubmitting}>
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 function fmtdate(commentdate) {
-    let date = new Date(commentdate)
+  let date = new Date(commentdate);
 
-    let formatted = new Intl.DateTimeFormat('en-US', {
-        // VALUES CAN BE "LONG", "SHORT", OR "NUMERIC"
+  let formatted = new Intl.DateTimeFormat("en-US", {
+    // VALUES CAN BE "LONG", "SHORT", OR "NUMERIC"
 
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
+    month: "short",
+    day: "numeric",
+    year: "numeric",
 
-        // 12 OR 24 HOURS TIME
-        hour12: true
-    }).format(date);
-    // console.log(formatted);
-    return formatted;
-
+    // 12 OR 24 HOURS TIME
+    hour12: true,
+  }).format(date);
+  // console.log(formatted);
+  return formatted;
 }
 
 function RenderDish(props) {
-    console.log(props)
-    console.log(props.dish.image)
-    return (
-        <Card>
-            <CardImg top src={props.dish.image} alt={props.dish.name} />
-            <CardBody>
-                <CardTitle>{props.dish.name}</CardTitle>
-                <CardText>{props.dish.description}</CardText>
-            </CardBody>
-        </Card>
-    );
+  console.log(props);
+  console.log(props.dish.image);
+  return (
+    <Card>
+      <CardImg top src={props.dish.image} alt={props.dish.name} />
+      <CardBody>
+        <CardTitle>{props.dish.name}</CardTitle>
+        <CardText>{props.dish.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function RenderComments({ comments }) {
-
-    if (comments != null) {
-        // console.log(comments);
-        const values = comments.map((comment) => {
-            return (
-                <div className="col-12 col-md-5 m-1">
-
-                                <div key={comment.id}>
-                                    <ul className="list-unstyled">
-                                        <li>{comment.comment}</li>
-                                        <li>--{comment.author}, {fmtdate(comment.date)}</li>
-                                    </ul>
-                                </div>
-
-
-
-
-                </div>
-
-            )
-        });
-        return (
-
-            values
-
-        );
-
-    }
-    else {
-        return (
-            <div>No Comments</div>);
-    }
+  if (comments != null) {
+    // console.log(comments);
+    const values = comments.map((comment) => {
+      return (
+        <div >
+          <div key={comment.id}>
+            <ul className="list-unstyled">
+              <li>{comment.comment}</li>
+              <li>
+                --{comment.author}, {fmtdate(comment.date)}
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    });
+    return values;
+  } else {
+    return <div>No Comments</div>;
+  }
 }
-
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
-        console.log(props);
-        return (
-            <div className="container">
-                <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>
-            </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
+  if (props.dish != null) {
+    console.log(props);
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-6 col-md-5 m-1">
+            <Card>
+              <CardBody>
+                <CardTitle>
+                  <h4>Comments</h4>
+                </CardTitle>
 
+                <CardText>
+                  <RenderComments comments={props.comments} />
+                  <CommentForm />
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardBody>
-                                <CardTitle><h4>Comments</h4></CardTitle>
-
-                                <CardText>
-                                <RenderComments comments={props.comments} />
-                                </CardText>
-                            </CardBody>
-                        </Card>
-
-
-
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div></div>
-        )
-    }
-
-}
-
-export default DishDetail
+export default DishDetail;
